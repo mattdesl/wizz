3bec5773d784a9ddf2e4b7ce9ec24b8fea1c6738
mattdesl <dave.des@gmail.com>
2015-02-04 13:15:52 -0500

    support for garnish

--- cmd.js
+++ cmd.js
@@ -18,9 +18,6 @@ var argv = minimist(args)
 
 //support custom cwd and index.html file
 argv.path = argv.path || argv.p || process.cwd()
-argv.index = (argv.index || argv.i)
-if (argv.index)
-    argv.index = path.join(argv.path, argv.index)
 
 argv.entries = argv._.map(function(arg) {
     if (arg.indexOf(':') === -1) 
@@ -46,23 +43,8 @@ wizz(argv, function(err, result) {
     }
 
     //fix up console logs.. 
-    console.log('server running at', uri)
-    var logger = console.log.bind(console)
-    console.log = function(d) {
-        var caught = false
-        if (typeof d==='string') {
-            try { 
-                d = JSON.parse(d)
-                if (d.url||d.time)
-                    caught = true
-                if (d.elapsed) 
-                    logger('bundled in', d.elapsed)
-            } catch(e) {}
-        }
-        if (!caught)
-            logger.apply(this, Array.prototype.slice.call(arguments))
-    }
-
+    console.log(JSON.stringify('server running at '+uri))
+    
     //really basic live-reload support
     argv.live = argv.live || argv.l
     if (argv.live) {
@@ -71,7 +53,7 @@ wizz(argv, function(err, result) {
 
         var server = tinylr()
         server.listen(argv.live, 'localhost', function(a) {
-            console.log('livereload running on port %s', argv.live);
+            console.log(JSON.stringify('livereload running on '+argv.live))
         })
 
         var watch = (argv.watch || argv.w) 
